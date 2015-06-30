664f69088470b94fa156564165ab03ae370430c8
Einar Ingebrigtsen <einar@dolittle.com>
2015-06-30 07:08:42 +0200

    Adding ability to create / join chatroom

--- SignalRChat.Mobile/SignalRChat.Mobile/ChatRoomsViewModel.cs
+++ SignalRChat.Mobile/SignalRChat.Mobile/ChatRoomsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace SignalRChat.Mobile
@@ -8,13 +9,17 @@ namespace SignalRChat.Mobile
     {
         string _currentRoom;
         INavigation _navigation;
+        IChatHub _chatHub;
 
         public ChatRoomsViewModel(INavigation navigation, IChatHub chatHub)
         {
             _navigation = navigation;
+            _chatHub = chatHub;
             Rooms = new ObservableCollection<string>();
 
             chatHub.RoomAdded += (room) => Device.BeginInvokeOnMainThread(() => Rooms.Add(room));
+
+            AddRoomCommand = DelegateCommand.Create(AddRoom);
         }
 
         public string CurrentRoom 
@@ -28,5 +33,14 @@ namespace SignalRChat.Mobile
         }
 
         public ObservableCollection<String> Rooms { get; private set; }
+
+        public ICommand AddRoomCommand { get; private set; }
+
+        public string Room { get; set; }
+
+        public void AddRoom()
+        {
+            _chatHub.CreateRoom(Room);
+        }
     }
 }
